fc637a95fae42128a21d7c9b84026a7d7a2ad92b
Henrique Miranda <josehmiranda22@gmail.com>
2021-06-05 15:39:50 -0300

    remoção de acento de prontuário

--- src/components/sidebar/sidebar.js
+++ src/components/sidebar/sidebar.js
@@ -20,25 +20,54 @@ export default function SideBarGlobal({isOpen, atualPage}) {
 
                     <ul className="nav-pills sidebar-nav-global">
                         <li>
-                            <Link to="/cliente" className={atualPage === client ? 'sidebar-list-global sidebar-active-global' : 'sidebar-list-global sidebar-inactive-global'} aria-current="page">
+                            <Link
+                                to="/cliente"
+                                className={
+                                    atualPage === client ? 
+                                    'sidebar-list-global sidebar-active-global' :
+                                    'sidebar-list-global sidebar-inactive-global'
+                                }
+                                aria-current="page"
+                            >
                                 <User size={16} style={{ marginRight: 10, marginBottom: 4 }} />
                                 {client}
                             </Link>
                         </li>
                         <li>
-                            <Link to="/especialista" className={atualPage === Specialist ? 'sidebar-list-global sidebar-active-global' : 'sidebar-list-global sidebar-inactive-global'}>
+                            <Link
+                                to="/especialista"
+                                className={
+                                    atualPage === Specialist ?
+                                    'sidebar-list-global sidebar-active-global' :
+                                    'sidebar-list-global sidebar-inactive-global'
+                                }
+                            >
                                 <Activity size={16} style={{ marginRight: 10, marginBottom: 4 }} />
                                 {Specialist}
                             </Link>
                         </li>
                         <li>
-                            <Link to="/atendimento" className={atualPage === medicalCare ? 'sidebar-list-global sidebar-active-global' : 'sidebar-list-global sidebar-inactive-global'}>
+                            <Link
+                                to="/atendimento"
+                                className={
+                                    atualPage === medicalCare ?
+                                    'sidebar-list-global sidebar-active-global' :
+                                    'sidebar-list-global sidebar-inactive-global'
+                                }
+                            >
                                 <Calendar size={16} style={{ marginRight: 10, marginBottom: 4 }} />
                                 {medicalCare}
                             </Link>
                         </li>
                         <li>
-                            <Link to="/prontuário" className={atualPage === medicalRecord ? 'sidebar-list-global sidebar-active-global' : 'sidebar-list-global sidebar-inactive-global'}>
+                            <Link
+                                to="/prontuario"
+                                className={
+                                    atualPage === medicalRecord ?
+                                    'sidebar-list-global sidebar-active-global' :
+                                    'sidebar-list-global sidebar-inactive-global'
+                                }
+                            >
                                 <Clipboard size={16} style={{ marginRight: 10, marginBottom: 4 }} />
                                 {medicalRecord}
                             </Link>
