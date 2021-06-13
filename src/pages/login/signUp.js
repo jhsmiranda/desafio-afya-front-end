050d588baf75237dac8e913e16da283126980267
fdutrac <fabiohenrique@gestaointeligentesofware.com.br>
2021-06-13 16:44:44 -0300

    Adiciona integração a api - cadastro

--- src/pages/login/signUp.js
+++ src/pages/login/signUp.js
@@ -1,4 +1,5 @@
-import React, { useEffect } from 'react'
+import axios from 'axios'
+import React, { useEffect, useState } from 'react'
 import { useHistory, Link } from 'react-router-dom'
 
 import Fundo from '../../assets/images/fundo1.png'
@@ -6,6 +7,21 @@ import Fundo from '../../assets/images/fundo1.png'
 import { BodyLogin, FilterImage, ContainerLogin, ContainerContent, ListLogin, Brand } from '../../styles/styleLogin'
 
 function Register() {
+  const [formData, setFormData] = useState({
+    name: '',
+    login: '',
+    password: '',
+  })
+
+  const handleFormDataName = (e) => {
+    setFormData({ ...formData, name: e.target.value })
+  }
+  const handleFormDataLogin = (e) => {
+    setFormData({ ...formData, login: e.target.value })
+  }
+  const handleFormDataPassword = (e) => {
+    setFormData({ ...formData, password: e.target.value })
+  }
 
   useEffect(() => {
     document.title = "Pomarola | Cadastro"
@@ -13,40 +29,47 @@ function Register() {
 
   let history = useHistory();
 
-  const handleLogin = () => {
-    history.push("/");
-  }
-
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    // Faz requisição a API
+    axios.post('https://clinica-pomarola-api.herokuapp.com/users', formData).then((res) => {
+      // Se bem sucedida, retorna os dados inseridos no res.data
+      console.log(res.data);
+  }).catch((err) => {
+    // Se contem erros, envia a mensagem de erro no err.respose.data[0].msg
+    console.log(err.response.data[0].msg);
+  })
+}
   return (
-      <BodyLogin>
-        <FilterImage/>
-        <img src={Fundo} alt="fundo do página"/>
-        <ContainerLogin>
-          <ContainerContent>
-            <ul>
-              <Link to="/">
-                <ListLogin>LOGIN</ListLogin>
-              </Link>
-              <Link to="/cadastro">
-                <ListLogin active>CADASTRAR</ListLogin>
-              </Link>
-            </ul>
-            <form>
-              <input type="text" placeholder="Nome Completo"></input>
-              <input type="text" placeholder="Nome de usuário"></input>
-              <input type="password" placeholder="Senha"></input>
-            </form>
-            <button onClick={handleLogin} style={{ marginTop: 30 }}>CADASTRAR</button>
-          </ContainerContent>
-          <ContainerContent>
-            <Brand>
-              <h1>Clínica Pomarola</h1>
-              <p>Cuidando de sua saúde,</p>
-              <p>onde você estiver.</p>
-            </Brand>
-          </ContainerContent>
-        </ContainerLogin>
-      </BodyLogin>
+    <BodyLogin>
+      <FilterImage/>
+      <img src={Fundo} alt="fundo do página"/>
+      <ContainerLogin>
+        <ContainerContent>
+          <ul>
+            <Link to="/">
+              <ListLogin>LOGIN</ListLogin>
+            </Link>
+            <Link to="/cadastro">
+              <ListLogin active>CADASTRAR</ListLogin>
+            </Link>
+          </ul>
+          <form>
+            <input type="text" value={formData.name} onChange={handleFormDataName}  placeholder="Nome Completo" required />
+            <input type="text" value={formData.login} onChange={handleFormDataLogin} maxLength="20" placeholder="Nome de usuário" required/>
+            <input type="password" value={formData.password} onChange={handleFormDataPassword} placeholder="Senha" required/>
+          </form>
+          <button onClick={handleSubmit} style={{ marginTop: 30 }}>CADASTRAR</button>
+        </ContainerContent>
+        <ContainerContent>
+          <Brand>
+            <h1>Clínica Pomarola</h1>
+            <p>Cuidando de sua saúde,</p>
+            <p>onde você estiver.</p>
+          </Brand>
+        </ContainerContent>
+      </ContainerLogin>
+    </BodyLogin>
   );
 }
 
