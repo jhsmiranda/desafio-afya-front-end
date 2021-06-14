2fe1fa6d0184bbd6a61b67bb52c3342b4eef716e
Henrique Miranda <josehmiranda22@gmail.com>
2021-06-14 17:57:16 -0300

    config express

--- server.js
+++ server.js
@@ -1,1 +1,20 @@
-console.log('batata')
+const express = require('express')
+
+const { resolve } = require('path')
+
+const app = express()
+
+app.use('/',
+    espress.static(
+        resolve(
+            __dirname,
+            './build'
+        )
+    ))
+
+app.
+    listen(process.env.PORT || 3000, (err) => {
+        if (err) { return console.log(err)}
+
+        console.log('Tudo funconando certinho')
+})
